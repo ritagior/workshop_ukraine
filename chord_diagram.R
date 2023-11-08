23560a2c2caf2a162dbc3a92290f242e92ac67e3
ritagior <rgiordano@gmx.com>
2023-11-08 12:10:38 +0000

    chord_diagram.R

---
+++ chord_diagram.R
@@ -0,0 +1,29 @@
+library(circlize)
+library(tidyverse)
+
+data <- read_csv("data/refugees_country.csv")
+
+
+## create colour palette
+
+
+## define layout
+
+
+## chord diagram
+chord(
+
+)
+
+## add labels
+
+circos.track(
+
+)
+
+
+## add title
+
+## add footnote
+
+## add a second footnote data source
