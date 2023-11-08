a8c452f03c6c72e3e9ea79ec5daaffedd03ad7a0
ritagior <rgiordano@gmx.com>
2023-11-08 11:31:00 +0000

    exercise

---
+++ exercise/exercise_1.R
@@ -0,0 +1,10 @@
+## In this exercise you will create a track with labels
+library(circlize)
+
+## Create five sectors
+
+## Initialize the layout
+
+## Create the track
+
+## Add the label using panel.fun and circos.text
---
+++ exercise/exercise_2.R
@@ -0,0 +1,11 @@
+## Exercise 2
+## Create a chord diagram
+
+library(tidyverse)
+library(circlize)
+
+data <- read_csv("data/refugees_country.csv")
+
+## modify the layout using circos.par()
+
+## chord diagram
