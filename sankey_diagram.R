4b2334bdc5d9c350e5a618b8749ad686ab6f5fa4
ritagior <rgiordano@gmx.com>
2023-11-09 10:08:32 +0000

    sankey_diagram.R: add script

---
+++ sankey_diagram.R
@@ -0,0 +1,15 @@
+## Sankey diagram
+library(tidyverse)
+library(networkD3)
+
+data <- read_csv("data/refugees_country.csv")
+
+### create links and nodes
+
+
+## create sankey diagram
+
+
+## create colour using d3
+
+## create sankey diagram with the new colour palette
