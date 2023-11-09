3940ba3b6adeccc68711d4ba8f9f8cb829afc17e
ritagior <rgiordano@gmx.com>
2023-11-09 15:59:30 +0000

    sankey_diagram.R: update code

--- sankey_diagram.R
+++ sankey_diagram.R
@@ -5,11 +5,16 @@ library(networkD3)
 data <- read_csv("data/refugees_country.csv")
 
 ### create links and nodes
+links <- data %>%
+  rename()
 
+nodes <- data.frame(
+
+)
 
 ## create sankey diagram
 
-
+sankeyNetwork()
 ## create colour using d3
 
 ## create sankey diagram with the new colour palette
